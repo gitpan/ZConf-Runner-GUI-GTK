9b1f7e1cb511b42308ef548cbe3c45f629269bb6
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2010-06-26 09:09:15 +0000

    Import of VVELOX/ZConf-Runner-GUI-GTK-0.0.2 from CPAN.
    gitpan-cpan-distribution: ZConf-Runner-GUI-GTK
    gitpan-cpan-version:      0.0.2
    gitpan-cpan-path:         VVELOX/ZConf-Runner-GUI-GTK-0.0.2.tar.gz
    gitpan-cpan-author:       VVELOX
    gitpan-cpan-maturity:     released

--- lib/ZConf/Runner/GUI/GTK.pm
+++ lib/ZConf/Runner/GUI/GTK.pm
@@ -14,11 +14,11 @@ ZConf::Runner::GUI::GTK - The GTK GUI backend for ZConf::Runner.
 
 =head1 VERSION
 
-Version 0.0.1
+Version 0.0.2
 
 =cut
 
-our $VERSION = '0.0.1';
+our $VERSION = '0.0.2';
 
 
 =head1 SYNOPSIS
